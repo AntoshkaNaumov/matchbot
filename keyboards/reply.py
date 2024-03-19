21b85e69c6a83afc57e20541fa5c611ff3cc37e5
Anton Naumov <99544445+AntoshkaNaumov@users.noreply.github.com>
2024-03-19 22:37:17 +0300

    Add files via upload

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -26,3 +26,29 @@ stat_menu = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True
 )
+
+gender_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Парень"),
+            KeyboardButton(text="Девушка"),
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+age_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="18-30 лет"),
+            KeyboardButton(text="31-40 лет"),
+        ],
+        [
+            KeyboardButton(text="41-50 лет"),
+            KeyboardButton(text=">50 лет"),
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
